1aa3d87a6cf857d1e5e010f91de068efb4883bfd
Francis Reyes <reyesfrancisp@gmail.com>
2023-06-23 14:12:36 -0400

    Added some examples of markdown generation in class

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile();
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    userPrompt = inquirer.createPromptModule();
+}
 
 // Function call to initialize app
 init();
